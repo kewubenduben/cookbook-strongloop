7d112ab6845982a6b5cd7534de2325dd8666baa8
Ker Ruben Ramos <xdiscent@gmail.com>
2013-10-11 06:09:54 +0800

    Fix recipes

--- recipes/install_from_binary.rb
+++ recipes/install_from_binary.rb
@@ -2,5 +2,6 @@ ark "strongloop" do
  url node['strongloop']['binary']
  version node['strongloop']['version']
  checksum node["strongloop"]["checksums"][File.basename(node['strongloop']['binary'])]
+ append_env_path true
  action :install
 end
--- recipes/install_from_package.rb
+++ recipes/install_from_package.rb
@@ -1,10 +1,10 @@
 case node['platform_family']
   when 'debian'
-    install_command = "dpkg -i --root='#{node[strongloop][dir]}'"
-    not_if_command = "dpkg --list 2>&1 | grep strongloop | grep #{node[strongloop][version]}"
+    install_command = "/usr/bin/dpkg -i"
+    not_if_command = "/usr/bin/dpkg --list 2>&1 | grep strongloop | grep #{node["strongloop"]["version"]}"
   when 'rhel'
-    install_command = "rpm -Uvh --root='#{node[strongloop][dir]}'"
-    not_if_command = "rpm -qa 2>&1 | grep strongloop | grep #{node[strongloop][version]}"
+    install_command = "/usr/bin/rpm -Uvh"
+    not_if_command = "/usr/bin/rpm -qa 2>&1 | grep strongloop | grep #{node["strongloop"]["version"]}"
   else
     Chef::Log.error "There are no strongloop packages for this platform; please use the source or binary method to install node"
     return
@@ -13,14 +13,15 @@ end
 bash "install-node" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOH
-     #{install_command} "#{::File.basename(node[strongloop][package])}"
+     #{install_command} "#{::File.basename(node["strongloop"]["package"])}"
   EOH
   action :nothing
-  not_if not_if_command
+  not_if "#{not_if_command}"
 end
 
 remote_file "#{Chef::Config[:file_cache_path]}/#{::File.basename(node['strongloop']['package'])}" do
   source node['strongloop']['package']
   checksum node["strongloop"]["checksums"][File.basename(node['strongloop']['package'])]
-  notifies :run, resources(:bash => "install-node"), :immediately
+  mode 00644
+  notifies :run, "bash[install-node]", :immediately
 end
