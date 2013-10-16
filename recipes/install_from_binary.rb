cb16235d38965d0e6de7d81e872fa162711b11ea
Ker Ruben Ramos <xdiscent@gmail.com>
2013-10-16 11:05:12 +0800

    npm has a broken path

--- recipes/install_from_binary.rb
+++ recipes/install_from_binary.rb
@@ -2,6 +2,7 @@ ark "strongloop" do
  url node['strongloop']['binary']
  version node['strongloop']['version']
  checksum node["strongloop"]["checksums"][File.basename(node['strongloop']['binary'])]
+ has_binaries %w{ bin/slc bin/slnode bin/node }
  append_env_path true
  action :install
 end
