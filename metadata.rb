000dd86082757c0e61ee2ade73277eb406007f76
Ker Ruben Ramos <xdiscent@gmail.com>
2013-11-30 08:05:57 +0800

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kramos@phewcentral.com'
 license          'All rights reserved'
 description      'Installs/Configures strongloop'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 
 %w{ debian ubuntu }.each {|os| supports os}
 
